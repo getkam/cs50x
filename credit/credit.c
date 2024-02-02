e69d46b078e62128356be036b5a078960c4e0fd9
getkam <87501819+getkam@users.noreply.github.com>
2024-02-02 21:56:33 +0000

    Fri, Feb 2, 2024, 10:56 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,22 +12,19 @@ int main(void)
     {
         number = get_long("Number: ");
     }
-    while(number < 0);
+    while (number < 0);
 
     bool checksum = verifyCheckSum(number);
     string code = getCardProvider(number);
 
-    printf("checksum %d\n", checksum);
-
     if (checksum == true)
     {
-       printf("%s", code);
+        printf("%s", code);
     }
     else
     {
         printf("INVALID\n");
     }
-
 }
 
 bool verifyCheckSum(long int number)
@@ -37,34 +34,31 @@ bool verifyCheckSum(long int number)
     int sumOfNOTMultiplied = 0;
 
     int index = 1;
-    for(long int remainingNo = number; remainingNo > 0;  remainingNo=remainingNo/10)
+    for (long int remainingNo = number; remainingNo > 0; remainingNo = remainingNo / 10)
     {
         int digit = remainingNo % 10;
 
-        if (index % 2 > 0) //not multiplied
+        if (index % 2 > 0)
         {
             sumOfNOTMultiplied = sumOfNOTMultiplied + digit;
         }
-        else
-        if(index % 2 == 0)//multiplied
+        else if (index % 2 == 0)
         {
-            mutiplied = digit*2;
+            mutiplied = digit * 2;
             int sumOfDigits = 0;
             do
             {
                 sumOfDigits = sumOfDigits + mutiplied % 10;
                 mutiplied = mutiplied / 10;
             }
-            while(mutiplied > 0);
+            while (mutiplied > 0);
             sumOfMultiplied = sumOfMultiplied + sumOfDigits;
         }
         index++;
     }
     int total = sumOfMultiplied + sumOfNOTMultiplied;
 
-    printf("total: %i\n", total);
-
-    return (total % 10 == 0)? true : false;
+    return (total % 10 == 0) ? true : false;
 }
 
 string getCardProvider(long int number)
@@ -72,23 +66,20 @@ string getCardProvider(long int number)
     int legth = 1;
     int firstTwoDigits = 0;
 
-    for(long int remainingNo = number; remainingNo > 10;  remainingNo=remainingNo/10)
+    for (long int remainingNo = number; remainingNo > 10; remainingNo = remainingNo / 10)
     {
         firstTwoDigits = remainingNo;
-        legth ++;
+        legth++;
     }
-//American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit
-    if((firstTwoDigits == 34 || firstTwoDigits == 37) && legth ==15)
+    if ((firstTwoDigits == 34 || firstTwoDigits == 37) && legth == 15)
     {
         return "AMEX\n";
     }
-    else
-    if(firstTwoDigits / 10 == 4 && (legth == 13 || legth == 16))
+    else if (firstTwoDigits / 10 == 4 && (legth == 13 || legth == 16))
     {
         return "VISA\n";
     }
-    else
-    if(firstTwoDigits >= 51 && firstTwoDigits <= 55 && legth == 16)
+    else if (firstTwoDigits >= 51 && firstTwoDigits <= 55 && legth == 16)
     {
         return "MASTERCARD\n";
     }
