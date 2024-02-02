1f9387667ae020477c06226ee32de09299837194
getkam <87501819+getkam@users.noreply.github.com>
2024-02-02 19:33:49 +0000

    Fri, Feb 2, 2024, 8:33 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,6 +8,19 @@ int main(void)
     do
     {
         number = get_long("Number: ");
-    }while(number<0);
+    }
+    while(number < 0);
+
+    int mutiplied;
+    long remainedNumber;
+
+    do
+    {
+       int rest = (int) remainedNumber % 10;
+       remainedNumber = remainedNumber / 10;
+       printf("rest: %i\n", rest);
+       printf("remained number: %l\n", remainedNumber);
+    }
+    while(remainedNumber > 0);
 }
 
