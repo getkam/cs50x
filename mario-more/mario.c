e8e231405a5977549c8cae16660a70738848994c
getkam <87501819+getkam@users.noreply.github.com>
2024-02-02 18:44:50 +0000

    Fri, Feb 2, 2024, 7:44 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,8 +9,9 @@ int main(void)
 {
     int height;
     do{
-height = get_int("Height: ");
-    }while(height > 0 && height < 9);
+        height = get_int("Height: ");
+    }while(height < 0 || height > 9);
+
     for (int i = 0; i < height; i++)
     {
         int spaces = height - i - 1;
