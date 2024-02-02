3ba49d6ee2b1e1dabb4583a0f62f0e0f8a65fd8d
getkam <87501819+getkam@users.noreply.github.com>
2024-02-02 18:38:31 +0000

    Fri, Feb 2, 2024, 7:38 PM +01:00

---
+++ mario-more/mario.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void printSpaces(int number);
+
+void printHashes(int number);
+
+int main(void)
+{
+  int height = get_int("Height: ");
+  if(height > 0 && height < 9)
+  {
+    for (int i = 0; i < height; i++)
+    {
+        int spaces = height - i - 1;
+        int hashes = i + 1;
+
+        printSpaces(spaces);
+        printHashes(hashes);
+        printf("  ");
+        printHashes(hashes);
+        printSpaces(spaces);
+        printf("\n");
+    }
+  }
+}
+
+void printSpaces(int number)
+{
+  for (int i = 0; i < number; i++)
+  {
+    printf(" ");
+  }
+}
+
+void printHashes(int number)
+{
+  for (int h = 0; h < number; h++)
+  {
+    printf("#");
+  }
+}
