47792b169325216bb9168e669526b83929b3a4ab
getkam <87501819+getkam@users.noreply.github.com>
2024-02-03 22:17:07 +0000

    Sat, Feb 3, 2024, 11:17 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,9 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-const int POINTS = []
+const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
+    string word1 = get_string("Player1: ");
+    string word2 = get_string("Player2: ");
+
+    int score1 = 
 
 }
