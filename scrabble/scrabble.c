5cbf6397e41c87fdfa818e850dc577f10f5908a9
getkam <87501819+getkam@users.noreply.github.com>
2024-02-03 22:21:58 +0000

    Sat, Feb 3, 2024, 11:21 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,7 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int calculate_score(string s);
+
 const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
 
 int main(void)
 {
@@ -10,6 +13,24 @@ int main(void)
 
     int score1 = calculate_score(word1);
     int score2 = calculate_score(word1);
+
+    if (score1 > score2)
+    {
+        printf("Player1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player2 wins!\n");
+    }
+    else if (score1 = score2)
+    {
+        printf("Tie!\n");
+    }
 }
 
 int calculate_score(string s)
+{
+
+
+return 1;
+}
