1bad1ffe4f8ba9229611d9e352aa5d25d52ce6df
getkam <87501819+getkam@users.noreply.github.com>
2024-02-03 22:52:22 +0000

    Sat, Feb 3, 2024, 11:52 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,12 +1,11 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 int calculate_score(string s);
 
 const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
 
 int main(void)
 {
@@ -34,17 +33,17 @@ int calculate_score(string s)
 {
     int score = 0;
 
-    for (int i = 0, l = strlen(s); i < l ; i++)
+    for (int i = 0, l = strlen(s); i < l; i++)
     {
-        if(s[i])
+        if (s[i])
         {
-            if(isupper(s[i]))
+            if (isupper(s[i]))
             {
-                score += POINTS[s[i]-'A'];
+                score += POINTS[s[i] - 'A'];
             }
             else if (islower(s[i]))
             {
-                score += POINTS[s[i]-'a'];
+                score += POINTS[s[i] - 'a'];
             }
         }
     }
