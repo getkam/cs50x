636c23293e513f593bbd45430009ca97dfdaf8ac
getkam <87501819+getkam@users.noreply.github.com>
2024-02-03 23:08:15 +0000

    Sun, Feb 4, 2024, 12:08 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,9 +4,32 @@
 #include <stdio.h>
 #include <string.h>
 
+int countLetters(string s);
+int countSentences(string s);
+
 int main(void)
 {
     string text  = get_string("Text: ");
 
-    
+    int lettersAmount = countLetters(text);
+    int sentencesAmount = countSentences(text);
+
+}
+
+int countLetters(string s)
+{
+    for (int i = 0, l = strlen(s); i < l; i++)
+    {
+        i++;
+    }
+    return i;
+}
+
+int countSentences(string s)
+{
+    for (int i = 0, l = strlen(s); i < l; i++)
+    {
+        if(s[i]=='.)
+    }
+    return i;
 }
