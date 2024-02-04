2a01ad4bc611a8b63958c81b9e681addc4991c0c
getkam <87501819+getkam@users.noreply.github.com>
2024-02-04 10:43:51 +0000

    Sun, Feb 4, 2024, 11:43 AM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -17,12 +17,12 @@ int main(int argc, string argv[])
     {
         string plaintext = get_string("plaintext: ");
         string key = argv[1];
-        printf("KEY: %s", key);
+        printf("KEY: %s\n", key);
         printf("ciphertext: ");
 
         for (int i = 0, l = strlen(plaintext); i < l; i++)
         {
-            
+            char cph = 
         }
     }
 }
