065a61347c7bf9031561ee8f369bb91a46872f2b
getkam <87501819+getkam@users.noreply.github.com>
2024-02-06 21:56:04 +0000

    Tue, Feb 6, 2024, 10:56 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -98,7 +98,14 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++) 
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
