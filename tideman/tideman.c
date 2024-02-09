94b6417bcd39a54b41be03a6dc4c95c1aecc140b
getkam <87501819+getkam@users.noreply.github.com>
2024-02-09 07:19:48 +0000

    Fri, Feb 9, 2024, 8:19 AM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -286,7 +286,7 @@ void merge(pair listToSort[], int leftIndex, int middle, int rightIndex)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-            for (int i = 0; i < candidate_count; i++)
+        for (int i = 0; i < candidate_count; i++)
         {
             visited[i] = false;
             currVistited[i] = false;
