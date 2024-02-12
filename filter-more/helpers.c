3104b66079a4b65f59f82d7fd1d7839df9063a44
getkam <87501819+getkam@users.noreply.github.com>
2024-02-12 13:42:48 +0000

    Mon, Feb 12, 2024, 2:42 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int avg = (image[i][j].rgbtRed * image[i][j].rgbtGreen * image[i][j].rgbtBlue) / 3
-            
+            image[i][j].rgbtRed = avg;
         }
     }
     return;
