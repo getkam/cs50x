b5868bf08edba057e96dcc4f17f4de25565776b4
getkam <87501819+getkam@users.noreply.github.com>
2024-02-13 13:26:26 +0000

    Tue, Feb 13, 2024, 2:26 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -95,13 +95,15 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
-    for ( int i = 0; i < 3; i++)
-    {
-            for ( int j = 0; j < 3; j++)
-        {
-                printf("%i", Gx[i][j]);
-        }
-        printf("\n");
-    }
+    // for ( int i = 0; i < 3; i++)
+    // {
+    //         for ( int j = 0; j < 3; j++)
+    //     {
+    //             printf("%i ", Gy[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    
     return;
 }
