c6e737ed831bf2888da828437f9f5e912340b19b
getkam <87501819+getkam@users.noreply.github.com>
2024-02-13 09:23:18 +0000

    Tue, Feb 13, 2024, 10:23 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -25,7 +25,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE temp = image[i][j];
-            //image[i][j] = image[height - 1 - i][j];
+            image[i][j] = image[height - 1 - i][j];
             image[height - 1 - i][j].rgbtRed = temp.rgbtBlue;
             image[height - 1 - i][j].rgbtGreen = temp.rgbtGreen;
             image[height - 1 - i][j].rgbtBlue = temp.rgbtGreen;
