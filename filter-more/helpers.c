fe7c9a4a60dee4cc4f0f72c0a9c03d4d5ab675b2
getkam <87501819+getkam@users.noreply.github.com>
2024-02-13 07:25:11 +0000

    Tue, Feb 13, 2024, 8:25 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -35,7 +35,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            double avg = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+            int tmp = round(avg);
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = (BYTE)round(tmp);
+        }
+    }
     return;
 }
 
