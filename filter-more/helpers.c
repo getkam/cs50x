dde3fbff8ae66399e44859c4cb12e9dd7190e320
getkam <87501819+getkam@users.noreply.github.com>
2024-02-13 09:36:20 +0000

    Tue, Feb 13, 2024, 10:36 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -22,7 +22,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j <= width / 2); j++)
+        for (int j = 0; j <= (width / 2); j++)
         {
 
             RGBTRIPLE temp = image[i][j];
