3b8cd585b3d8aaa9f80a6582c98352628c939a92
getkam <87501819+getkam@users.noreply.github.com>
2024-02-13 12:36:23 +0000

    Tue, Feb 13, 2024, 1:36 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -54,7 +54,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int k = j - 1; k < j + 2; k++)
                 {
-                    if ((n > 0 && (n < (height - 1))) && (k > 0 && (k < (width - 1))))
+                    if ((n >= 0 && (n <= (height - 1))) && (k >= 0 && (k <= (width - 1))))
                     {
                         //printf("I'm in inside loop .......................\n");
                         //printf("index: %i\n", index);
