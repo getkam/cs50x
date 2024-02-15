5c0042532a450adf02ccc9ee94505ef6a56dd39b
getkam <87501819+getkam@users.noreply.github.com>
2024-02-15 19:01:29 +0000

    Thu, Feb 15, 2024, 8:01 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
         {
             if(isFileOpened) //previous file is still open - close it and open new one
             {
-                
+                fclose(fileJPG);
             }
             else // no file is opened - first finding
             {
