b197d039761f736d0bee6075b56329d43510afd8
getkam <87501819+getkam@users.noreply.github.com>
2024-02-15 16:20:11 +0000

    Thu, Feb 15, 2024, 5:20 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+
+//Acccept command line
+
+//open memeory card
+
+//while there are still signs to read
+    //create a new file .jpeg
+
+
 }
