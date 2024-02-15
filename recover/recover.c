035549255382a0b11d95ec697c9b868717e34119
getkam <87501819+getkam@users.noreply.github.com>
2024-02-15 19:35:31 +0000

    Thu, Feb 15, 2024, 8:35 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,18 +1,18 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
 
-    //Acccept command line
+    // Acccept command line
     if (argc != 2)
     {
         printf("Usage: ./recover FILE\n");
         return 1;
     }
-    //open memeory card
+    // open memeory card
     FILE *card = fopen(argv[1], "r");
     if (card == NULL)
     {
@@ -22,18 +22,20 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[512];
 
-    //while there are still signs to read
-  int fileNo = 0;
-  bool isFileOpened = false;
-  FILE *fileJPG;
-  char *fileName;
-    while(fread(buffer, 1, 512, card) == 512 && fileNo < 4)
+    // while there are still signs to read
+    int fileNo = 0;
+    bool isFileOpened = false;
+    FILE *fileJPG;
+    char *fileName;
+
+    int in
+    while (fread(buffer, 1, 512, card) == 512 && fileNo < 4)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             fileName = malloc(8);
             sprintf(fileName, "%03d.jpg", fileNo);
-            if(isFileOpened) //previous file is still open - close it and open new one
+            if (isFileOpened) // previous file is still open - close it and open new one
             {
                 fclose(fileJPG);
                 fileJPG = fopen(fileName, "w");
@@ -45,36 +47,37 @@ int main(int argc, char *argv[])
                 fileNo++;
             }
 
-            //write to the file
+            // write to the file
             fwrite(buffer, sizeof(uint8_t), 512, fileJPG);
 
-            //printf("-----------------------------------------------New file. S = %i\n",s);
+            // printf("-----------------------------------------------New file. S = %i\n",s);
             for (int i = 0; i < 512; i++) // print buffer
             {
                 printf("%02x ", buffer[i]);
-                if ((i + 1) % 16 == 0) {
+                if ((i + 1) % 16 == 0)
+                {
                     printf("\n");
                 }
             }
 
-        printf("\n");
-        printf("\n");
-        printf("\n");
-        printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
         }
 
-        if(isFileOpened)
+        if (isFileOpened)
         {
-            //write to the file
+            // write to the file
             fwrite(buffer, sizeof(uint8_t), 512, fileJPG);
         }
     }
-        if(isFileOpened)
-        {
-            fclose(fileJPG);
-        }
+    if (isFileOpened)
+    {
+        fclose(fileJPG);
+    }
 
-    //create a new file .jpeg
+    // create a new file .jpeg
     free(fileName);
     fclose(card);
 }
