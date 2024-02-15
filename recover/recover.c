ab2b39481a1183e62f742d2532fed9ba0e16307c
getkam <87501819+getkam@users.noreply.github.com>
2024-02-15 16:22:27 +0000

    Thu, Feb 15, 2024, 5:22 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,9 +4,14 @@
 int main(int argc, char *argv[])
 {
 
-//Acccept command line
-
+    //Acccept command line
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
 //open memeory card
+    FILE *card = fopen()
 
 //while there are still signs to read
     //create a new file .jpeg
