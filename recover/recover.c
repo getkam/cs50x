80cd35b4105a4ec8cf7150e1c50e28b3cee3385a
getkam <87501819+getkam@users.noreply.github.com>
2024-02-15 16:25:06 +0000

    Thu, Feb 15, 2024, 5:25 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,7 +11,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 //open memeory card
-    FILE *card = fopen()
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        printf("Could not open %s.\n", argv[1]);
+        return 1;
+    }
 
 //while there are still signs to read
     //create a new file .jpeg
