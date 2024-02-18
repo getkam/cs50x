dc55d917a27ff7f5268ce3455c2a444b11768bd0
getkam <87501819+getkam@users.noreply.github.com>
2024-02-18 12:36:11 +0000

    Sun, Feb 18, 2024, 1:36 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -79,7 +79,7 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if(p->)
+    if()
     // TODO: Free parents recursively
 
     // TODO: Free child
