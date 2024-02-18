bc059de3cf4d5b42ca0092eeba4e074fa28cc309
getkam <87501819+getkam@users.noreply.github.com>
2024-02-18 12:44:03 +0000

    Sun, Feb 18, 2024, 1:44 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,7 @@ person *create_family(int generations)
 {
 
     // TODO: Allocate memory for new person
-    person *current = malloc(1 * sizeof(person));
+    person *current = malloc(sizeof(person));
     // If there are still generations left to create
     if (generations > 1)
     {
