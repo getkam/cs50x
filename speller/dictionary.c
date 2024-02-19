59bf292a8a5921fdff0999863e2bb6a1db88964f
getkam <87501819+getkam@users.noreply.github.com>
2024-02-19 19:11:13 +0000

    Mon, Feb 19, 2024, 8:11 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -14,7 +14,7 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26 * 26; //two first letters
+const unsigned int N = 26; 
 
 // Hash table
 node *table[N];
