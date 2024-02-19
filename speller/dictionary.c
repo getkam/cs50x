c11ff94b5a16c8a816071d19845b9fd80971b959
getkam <87501819+getkam@users.noreply.github.com>
2024-02-19 21:41:38 +0000

    Mon, Feb 19, 2024, 10:41 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -64,10 +65,9 @@ bool load(const char *dictionary)
          unsigned int index = hash(word);
         if (table[index] == NULL)
         {
-            node *new = malloc(sizeof(node));
-            new->word = word;
-            new->next = NULL;
-            table[index] = new;
+            table[index] = malloc(sizeof(node));
+            strcpy(table[index]->word, word);
+            table[index]->next = NULL;
             dikiSize++;
         }
         else
