7fbcae6e9979c22f38a528422a07e9b008169b88
getkam <87501819+getkam@users.noreply.github.com>
2024-02-22 20:50:35 +0000

    Thu, Feb 22, 2024, 9:50 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,13 +1,24 @@
 from cs50 import get_s
 import re
 
-ccnumber = 0
-while ccnumber > 0:
-    ccnumber = input("Number: ")
+def main():
+    ccnumber = 0
+    while ccnumber > 0:
+        ccnumber = get_int("Number: ")
 
+    isValid = checkSum(ccnumber)
 
-re.match(ccnumber, [0-9]{13,15}
+    re.match(ccnumber, [0-9]{13,15}
 
 
 def checkSum(number):
+    digit = number % 10
+    remainingNo = number / 10
 
+    for index in range(len(number)):
+        
+
+    return True
+
+
+main()
