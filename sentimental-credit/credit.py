7215764318ffab1e118769e044237ae6049a18e6
getkam <87501819+getkam@users.noreply.github.com>
2024-02-22 21:49:56 +0000

    Thu, Feb 22, 2024, 10:49 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,8 +2,8 @@ from cs50 import get_string
 import re
 
 def main():
-    ccnumber = 0
-    while ccnumber < 1:
+    ccnumber = ""
+    while re.match(r'^\d{13,16}$', ccnumber):
         ccnumber = get_string("Number: ")
 
     if checkSum(ccnumber) == True:
@@ -18,8 +18,8 @@ def checkSum(number):
     sumNotMultiplied = 0
     sumOfMultiipled = 0
 
-    for index in range(len(str(number))):
-        digit  = remainingNo % 10
+    for index in range(len(number)):
+        digit  = int(number[len(number) - 1 - index])
 
         if index % 2 == 0:
             sumNotMultiplied += digit
@@ -40,11 +40,10 @@ def checkSum(number):
 
 def printCardProvider(number):
     pattern = r'^\d{13,16}$'
-    number = str(number)
-    begining2digits = int(str(number)[:2])
+    begining2digits = int(number[:2])
     if re.match(r'^\d{15}$', number) and (begining2digits == 34 or begining2digits == 37):
         print("AMEX")
-    elif (re.match(r'^\d{13}$', number) or re.match(r'^\d{16}$', number)) and round(begining2digits / 10) == 4:
+    elif (re.match(r'^\d{13}$', number) or re.match(r'^\d{16}$', number)) and number[0] == '4':
         print("VISA")
     elif re.match(r'^\d{16}$', number) and (begining2digits >=51 and begining2digits <=55):
         print("MASTERCARD")
