b9bfa4d7b5b36f7b6ba3b54f5ffd03c50a986996
getkam <87501819+getkam@users.noreply.github.com>
2024-02-22 20:17:50 +0000

    Thu, Feb 22, 2024, 9:17 PM +01:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,5 +2,12 @@ from cs50 import get_int
 
 height = 0
 
-while height < 0 or height >=8:
+while height < 1 or height > 8:
     height = get_int("Height: ")
+
+spaces = height - 1
+hashes = 1
+for i in range(height):
+    print(" " * spaces + "#" * hashes + "  " + "#" * hashes)
+    hashes += 1
+    spaces -= 1
