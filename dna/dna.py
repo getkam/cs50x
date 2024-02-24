b31aba8b23d3732da3751b8f450f376fce83bcc6
getkam <87501819+getkam@users.noreply.github.com>
2024-02-24 22:39:41 +0000

    Sat, Feb 24, 2024, 11:39 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,8 +5,8 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if (sys.argv != 2)
-        printf("Usage: ./dna dictionary")
+    if (sys.argv != 2):
+        print("Usage: ./dna database sequence")
 
     # TODO: Read database file into a variable
 
