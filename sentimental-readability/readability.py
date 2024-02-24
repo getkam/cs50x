78852f25d6b28df45595a47958d600eebd3f0e27
getkam <87501819+getkam@users.noreply.github.com>
2024-02-24 21:32:08 +0000

    Sat, Feb 24, 2024, 10:32 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,13 +1,21 @@
 from cs50 import get_string
 
-def main()
+def main():
     text = get_string("Text: ")
 
     letters = sum(c.isalpha() for c in text)
     words = text.count(" ") + 1
     sentences = text.count(".") + text.count("?") + text.count("!")
 
-    
+    L = (letters / words) * 100
+    S = (sentences / words) * 100
     index = 0.0588 * L - 0.296 * S - 15.8
+
+    if(index < 1):
+        print("Before Grade 1")
+    elif(index >16):
+        print("Grade 16+")
+    else:
+        print("Grade", round(index))
 
 main()
