6620348e2836369034b240ec54656bfd387c13b9
getkam <87501819+getkam@users.noreply.github.com>
2024-02-24 21:09:55 +0000

    Sat, Feb 24, 2024, 10:09 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,12 +6,23 @@ def main()
 
 
 def calculateLetters(text):
-    count = 0;
+    count = 0
     for char in text:
         if char.isalpha():
-            count +=1
+            count += 1
     return count
 
 def calculateWords(text):
+    count = 0
+    for char in text:
+        if char.isspace():
+            count += 1
+    count += 1 #for last word
+    return count
+
+def calculateSentences(text):
+    count = 0
+    for char in text:
+        
 
 main()
