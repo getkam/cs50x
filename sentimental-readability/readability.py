7ef4655df5470ec44de46953e01579e2cf7612ad
getkam <87501819+getkam@users.noreply.github.com>
2024-02-24 21:05:58 +0000

    Sat, Feb 24, 2024, 10:05 PM +01:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,17 @@
+from cs50 import get_string
+
+def main()
+
+
+
+
+def calculateLetters(text):
+    count = 0;
+    for char in text:
+        if char.isalpha():
+            count +=1
+    return count
+
+def calculateWords(text):
+
+main()
