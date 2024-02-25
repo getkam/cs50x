f21f823ddf18520abc7c15c70b11b25a615dcd3e
getkam <87501819+getkam@users.noreply.github.com>
2024-02-25 21:26:47 +0000

    Sun, Feb 25, 2024, 10:26 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -26,7 +26,6 @@ def main():
     for str in reader.fieldnames[1:]:
         matches[str] = longest_match(sequence, str)
 
-    print(matches)
     # TODO: Check database for matching profiles
     for entry in database:
         if all(int(entry[k]) == v for k, v in matches.items()):
