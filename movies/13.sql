2c4ac218fd0babf43b05da5dc540ce928a3b326a
getkam <87501819+getkam@users.noreply.github.com>
2024-02-28 19:45:03 +0000

    Wed, Feb 28, 2024, 8:45 PM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,4 @@
-SELECT s.movie_id FROM stars s JOIN people p ON s.person_id = p.id WHERE p.name = "Kevin Bacon" AND p.birth = 1958;
+SELECT DISTINCT(pp.name) FROM people pp JOIN stars ss ON pp.id = ss.person_id WHERE ss IN (
+SELECT s.movie_id FROM stars s JOIN people p ON s.person_id = p.id WHERE p.name = "Kevin Bacon" AND p.birth = 1958)
+AND pp.id NOT IN (SELECT id FROM people WHERE p.name = "Kevin Bacon" AND p.birth = 1958);
+
