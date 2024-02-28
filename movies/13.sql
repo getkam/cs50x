ff9a869b5e6aa4a041bcadbd5adb6d86ee88d95f
getkam <87501819+getkam@users.noreply.github.com>
2024-02-28 19:39:59 +0000

    Wed, Feb 28, 2024, 8:39 PM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958;
+SELECT s.movie_id FROM stars s JOIN people p ON s.person_id = p.id WHERE p.name = "Kevin Bacon" AND p.birth = 1958;
