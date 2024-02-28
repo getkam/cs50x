46a66a36764675ced954f8bd6e371f469010ea84
getkam <87501819+getkam@users.noreply.github.com>
2024-02-28 22:46:06 +0000

    Wed, Feb 28, 2024, 11:46 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,30 +1,29 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
 -- Kick off query - 3 witness , bakery, 10:15
-SELECT *
+SELECT
+  *
 FROM
   crime_scene_reports
 WHERE
-  year = 2023
-  AND month = 7
-  AND day = 28
+  YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 28
   AND street = "Humphrey Street";
 
-
-  --Interviews - valid informations are from
-    --> Ruth: car left parking up to 10 min after theft)
-    --> Eugene: she saw thief before 10:15 by ATM on Leggett Street withdrawing money
-    --> Raymond: thief call to someone right after 10:15, call lasted less than 1 min,
-        --other person suppose to buy a ticket on the erliers flight 29th July
-  SELECT
-    name,
-    transcript
-  FROM
-    interviews
-  WHERE
-   year = 2023
-   AND month = 7
-   AND day = 28;
+--Interviews - valid informations are from
+-- Ruth: car left parking up to 10 min after theft)
+-- Eugene: she saw thief before 10:15 by ATM on Leggett Street withdrawing money
+-- Raymond: thief call to someone right after 10:15, call lasted less than 1 min,
+-- other person suppose to buy a ticket on the erliers flight 29th July
+SELECT
+  name,
+  transcript
+FROM
+  interviews
+WHERE
+  YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 28;
 
 -- backery security logs ------------> licence plates
 SELECT
@@ -33,11 +32,11 @@ FROM
   bakery_security_logs
 WHERE
   activity = "exit"
-  AND year = 2023
-  AND month = 7
-  AND day = 28
-  AND hour = 10
-  AND minute BETWEEN 15 AND 26;
+  AND YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 28
+  AND HOUR = 10
+  AND MINUTE BETWEEN 15 AND 26;
 
 ---ATM records ----------------------> account_number, amount
 SELECT
@@ -45,13 +44,12 @@ SELECT
 FROM
   atm_transactions
 WHERE
-   year = 2023
-   AND month = 7
-   AND day = 28
-   AND atm_location = "Leggett Street"
-   AND transaction_type = "withdraw";
-
-   -----------------------------------------------list of people having this bank account
+  YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 28
+  AND atm_location = "Leggett Street"
+  AND transaction_type = "withdraw";
+-----------------------------------------------list of people having this bank account
 SELECT
   person_id
 FROM
@@ -60,15 +58,15 @@ WHERE
   account_number IN (
     SELECT
       account_number
-        FROM
-          atm_transactions
-        WHERE
-          year = 2023
-          AND month = 7
-          AND day = 28
-          AND atm_location = "Leggett Street"
-          AND transaction_type = "withdraw"
-    );
+    FROM
+      atm_transactions
+    WHERE
+      YEAR = 2023
+      AND MONTH = 7
+      AND DAY = 28
+      AND atm_location = "Leggett Street"
+      AND transaction_type = "withdraw"
+  );
 
 -- PHONE CALLS ------------------> caller and reciever numbers
 SELECT
@@ -76,30 +74,33 @@ SELECT
 FROM
   phone_calls
 WHERE
- year = 2023
- AND month = 7
- AND day = 28
- and duration < 60;
+  YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 28
+  AND duration < 60;
 
- ---> Earliest flight from Fiftyville on 29th of JULY
- SELECT
-   *
- FROM
-   flights
- WHERE
-   year = 2023
-   AND month = 7
-   AND day = 29
-   AND origin_airport_id IN (
-     SELECT id
-     FROM airports
-     WHERE city = "Fiftyville"
-   )
-   ORDER BY
-     hour,
-     minute
-   LIMIT
-     1;
+---> Earliest flight from Fiftyville on 29th of JULY
+SELECT
+  *
+FROM
+  flights
+WHERE
+  YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 29
+  AND origin_airport_id IN (
+    SELECT
+      id
+    FROM
+      airports
+    WHERE
+      city = "Fiftyville"
+  )
+ORDER BY
+  HOUR,
+  MINUTE
+LIMIT
+  1;
 
 --- DESTINATION CITY of out thiefs.  --------------- New York City
 SELECT
@@ -107,29 +108,31 @@ SELECT
 FROM
   airports
 WHERE
- id IN (
-  SELECT
-   destination_airport_id
- FROM
-   flights
- WHERE
-   year = 2023
-   AND month = 7
-   AND day = 29
-   AND origin_airport_id IN (
-     SELECT id
-     FROM airports
-     WHERE city = "Fiftyville"
-   )
-   ORDER BY
-     hour,
-     minute
-   LIMIT
-     1
-);
+  id IN (
+    SELECT
+      destination_airport_id
+    FROM
+      flights
+    WHERE
+      YEAR = 2023
+      AND MONTH = 7
+      AND DAY = 29
+      AND origin_airport_id IN (
+        SELECT
+          id
+        FROM
+          airports
+        WHERE
+          city = "Fiftyville"
+      )
+    ORDER BY
+      HOUR,
+      MINUTE
+    LIMIT
+      1
+  );
 
 ------------------------------- list of passport numbers
-
 SELECT
   passport_number
 FROM
@@ -141,44 +144,48 @@ WHERE
     FROM
       flights
     WHERE
-    year = 2023
-    AND month = 7
-    AND day = 29
-    AND origin_airport_id IN (
-        SELECT id
-        FROM airports
-        WHERE city = "Fiftyville"
-    )
+      YEAR = 2023
+      AND MONTH = 7
+      AND DAY = 29
+      AND origin_airport_id IN (
+        SELECT
+          id
+        FROM
+          airports
+        WHERE
+          city = "Fiftyville"
+      )
     ORDER BY
-        hour,
-        minute
+      HOUR,
+      MINUTE
     LIMIT
-        1
-  )
---------------------------------------- Thief
+      1
+  );
+
+--------------------------------------- Thief = BORIS
 SELECT
   *
 FROM
   people
 WHERE
   id IN (
-SELECT
-  person_id
-FROM
-  bank_accounts
-WHERE
-  account_number IN (
     SELECT
-      account_number
+      person_id
+    FROM
+      bank_accounts
+    WHERE
+      account_number IN (
+        SELECT
+          account_number
         FROM
           atm_transactions
         WHERE
-          year = 2023
-          AND month = 7
-          AND day = 28
+          YEAR = 2023
+          AND MONTH = 7
+          AND DAY = 28
           AND atm_location = "Leggett Street"
           AND transaction_type = "withdraw"
-    )
+      )
   )
   AND phone_number IN (
     SELECT
@@ -186,10 +193,10 @@ WHERE
     FROM
       phone_calls
     WHERE
-    year = 2023
-    AND month = 7
-    AND day = 28
-    and duration < 60
+      YEAR = 2023
+      AND MONTH = 7
+      AND DAY = 28
+      AND duration < 60
   )
   AND passport_number IN (
     SELECT
@@ -199,24 +206,27 @@ WHERE
     WHERE
       flight_id IN (
         SELECT
-        id
+          id
         FROM
-        flights
+          flights
         WHERE
-        year = 2023
-        AND month = 7
-        AND day = 29
-        AND origin_airport_id IN (
-            SELECT id
-            FROM airports
-            WHERE city = "Fiftyville"
-        )
+          YEAR = 2023
+          AND MONTH = 7
+          AND DAY = 29
+          AND origin_airport_id IN (
+            SELECT
+              id
+            FROM
+              airports
+            WHERE
+              city = "Fiftyville"
+          )
         ORDER BY
-            hour,
-            minute
+          HOUR,
+          MINUTE
         LIMIT
-            1
-    )
+          1
+      )
   )
   AND license_plate IN (
     SELECT
@@ -225,178 +235,101 @@ WHERE
       bakery_security_logs
     WHERE
       activity = "exit"
-      AND year = 2023
-      AND month = 7
-      AND day = 28
-      AND hour = 10
-      AND minute BETWEEN 15 AND 26
+      AND YEAR = 2023
+      AND MONTH = 7
+      AND DAY = 28
+      AND HOUR = 10
+      AND MINUTE BETWEEN 15 AND 26
   );
 
-  ----------------------------------------------------- ACCOMPLICE
-
+----------------------------------------------------- ACCOMPLICE = ROBIN
 SELECT
   name
 FROM
- people pp
- JOIN phone_calls pc ON pp.phone_number = pc.receiver
+  people pp
+  JOIN phone_calls pc ON pp.phone_number = pc.receiver
 WHERE
-  year = 2023
-  AND month = 7
-  AND day = 28
+  YEAR = 2023
+  AND MONTH = 7
+  AND DAY = 28
   AND duration < 60
   AND pc.caller IN (
     SELECT
-    phone_number --phone number of a thief
+      phone_number --phone number of a thief
     FROM
-    people
+      people
     WHERE
-    id IN (
-    SELECT
-    person_id
-    FROM
-    bank_accounts
-    WHERE
-    account_number IN (
+      id IN (
         SELECT
-        account_number
-            FROM
-            atm_transactions
-            WHERE
-            year = 2023
-            AND month = 7
-            AND day = 28
-            AND atm_location = "Leggett Street"
-            AND transaction_type = "withdraw"
-        )
-    )
-    AND phone_number IN (
-        SELECT
-        caller
+          person_id
         FROM
-        phone_calls
+          bank_accounts
         WHERE
-        year = 2023
-        AND month = 7
-        AND day = 28
-        and duration < 60
-    )
-    AND passport_number IN (
-        SELECT
-        passport_number
-        FROM
-        passengers
-        WHERE
-        flight_id IN (
+          account_number IN (
             SELECT
-            id
+              account_number
             FROM
-            flights
+              atm_transactions
             WHERE
-            year = 2023
-            AND month = 7
-            AND day = 29
-            AND origin_airport_id IN (
-                SELECT id
-                FROM airports
-                WHERE city = "Fiftyville"
-            )
+              YEAR = 2023
+              AND MONTH = 7
+              AND DAY = 28
+              AND atm_location = "Leggett Street"
+              AND transaction_type = "withdraw"
+          )
+      )
+      AND phone_number IN (
+        SELECT
+          caller
+        FROM
+          phone_calls
+        WHERE
+          YEAR = 2023
+          AND MONTH = 7
+          AND DAY = 28
+          AND duration < 60
+      )
+      AND passport_number IN (
+        SELECT
+          passport_number
+        FROM
+          passengers
+        WHERE
+          flight_id IN (
+            SELECT
+              id
+            FROM
+              flights
+            WHERE
+              YEAR = 2023
+              AND MONTH = 7
+              AND DAY = 29
+              AND origin_airport_id IN (
+                SELECT
+                  id
+                FROM
+                  airports
+                WHERE
+                  city = "Fiftyville"
+              )
             ORDER BY
-                hour,
-                minute
+              HOUR,
+              MINUTE
             LIMIT
-                1
-        )
-    )
-    AND license_plate IN (
+              1
+          )
+      )
+      AND license_plate IN (
         SELECT
-        license_plate
+          license_plate
         FROM
-        bakery_security_logs
+          bakery_security_logs
         WHERE
-        activity = "exit"
-        AND year = 2023
-        AND month = 7
-        AND day = 28
-        AND hour = 10
-        AND minute BETWEEN 15 AND 26
-    )
-
-
- )
-
-
-SELECT
-  phone_number --phone number of a thief
-FROM
-  people
-WHERE
-  id IN (
-SELECT
-  person_id
-FROM
-  bank_accounts
-WHERE
-  account_number IN (
-    SELECT
-      account_number
-        FROM
-          atm_transactions
-        WHERE
-          year = 2023
-          AND month = 7
-          AND day = 28
-          AND atm_location = "Leggett Street"
-          AND transaction_type = "withdraw"
-    )
-  )
-  AND phone_number IN (
-    SELECT
-      caller
-    FROM
-      phone_calls
-    WHERE
-    year = 2023
-    AND month = 7
-    AND day = 28
-    and duration < 60
-  )
-  AND passport_number IN (
-    SELECT
-      passport_number
-    FROM
-      passengers
-    WHERE
-      flight_id IN (
-        SELECT
-        id
-        FROM
-        flights
-        WHERE
-        year = 2023
-        AND month = 7
-        AND day = 29
-        AND origin_airport_id IN (
-            SELECT id
-            FROM airports
-            WHERE city = "Fiftyville"
-        )
-        ORDER BY
-            hour,
-            minute
-        LIMIT
-            1
-    )
-  )
-  AND license_plate IN (
-    SELECT
-      license_plate
-    FROM
-      bakery_security_logs
-    WHERE
-      activity = "exit"
-      AND year = 2023
-      AND month = 7
-      AND day = 28
-      AND hour = 10
-      AND minute BETWEEN 15 AND 26
+          activity = "exit"
+          AND YEAR = 2023
+          AND MONTH = 7
+          AND DAY = 28
+          AND HOUR = 10
+          AND MINUTE BETWEEN 15 AND 26
+      )
   )
