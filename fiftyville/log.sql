bb5dc491151513e21525e610487006db74fd9a8a
getkam <87501819+getkam@users.noreply.github.com>
2024-02-28 20:43:11 +0000

    Wed, Feb 28, 2024, 9:43 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Kick off query
+SELECT *
+FROM
+  crime_scene_reports
+WHERE
+  year = 2023
+  AND month = 7
+  AND day = 28
+  AND street = "Humphrey Street";
