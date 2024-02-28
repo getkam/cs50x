ff4cb91fbaad10cd7fc4d598de48cfc25e956e15
getkam <87501819+getkam@users.noreply.github.com>
2024-02-28 19:25:07 +0000

    Wed, Feb 28, 2024, 8:25 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,22 @@
-SELECT m.title FROM movies m WHERE m.id IN(
-SELECT s.movie_id FROM people p  JOIN stars s ON s.person_id = p.id WHERE p.name = "Bradley Cooper"
-INTERSECT
-SELECT s.movie_id FROM people p  JOIN stars s ON s.person_id = p.id WHERE p.name = "Jennifer Lawrence");
+SELECT
+  m.title
+FROM
+  movies m
+WHERE
+  m.id IN (
+    SELECT
+      s.movie_id
+    FROM
+      people p
+      JOIN stars s ON s.person_id = p.id
+    WHERE
+      p.name = "Bradley Cooper"
+    INTERSECT
+    SELECT
+      s.movie_id
+    FROM
+      people p
+      JOIN stars s ON s.person_id = p.id
+    WHERE
+      p.name = "Jennifer Lawrence"
+  );
