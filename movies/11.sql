1e314361e577eb27527cab88bd44e995a63eecf4
getkam <87501819+getkam@users.noreply.github.com>
2024-02-28 18:52:49 +0000

    Wed, Feb 28, 2024, 7:52 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -6,13 +6,11 @@ FROM
 WHERE
   m.id IN (
    SELECT
-     s.id
+     s.movie_id
    FROM
-     starts s
+     stars s
      JOIN people p ON s.person_id = p.id
      WHERE
-       name = "Chadwick Boseman")
+       p.name = "Chadwick Boseman")
 ORDER BY r.rating DESC
 LIMIT 5;
-
-SELECT m.title FROM movies m JOIN ratings r ON m.id = r.movie_id WHERE m.id IN (SELECT s.id FROM starts s JOIN people p ON s.person_id = p.id WHERE name = "Chadwick Boseman") ORDER BY r.rating DESC LIMIT 5;
