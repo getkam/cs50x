1b7b069bb8ea18da340f9b2697538ee9af0b472c
getkam <87501819+getkam@users.noreply.github.com>
2024-03-03 10:24:48 +0000

    Sun, Mar 3, 2024, 11:24 AM +01:00

--- finance/static/scripts.js
+++ finance/static/scripts.js
@@ -3,8 +3,8 @@ function validatePassword(){
     let pass = document.getElementById("password").value
     let pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    if (!pattern.test(pass))
+    if (pass)
     {
-        alert("Password doesn't match criteria");
+        document.getElementById("errorPasswordValidation").innerTest = "Password doesn't match criteria"
     }
 }
