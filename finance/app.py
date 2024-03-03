efb7d206ff3e191b0fb9dd35104b627568286ba6
getkam <87501819+getkam@users.noreply.github.com>
2024-03-03 11:44:07 +0000

    Sun, Mar 3, 2024, 12:44 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -125,8 +125,8 @@ def register():
         confirmpassword = request.form.get("confirmpassword")
         if password !=confirmpassword :
             return apology("passwords are not the same", 403)
-        
-        db.execute("INSERT INTO users (username, hash) VALUES (?,?)",username, password)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?,?);",username, password)
         return render_template("index.html")
     if request.method == "GET":
         return render_template("register.html")
