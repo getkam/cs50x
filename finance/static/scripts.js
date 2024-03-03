4e370e812421fb8c2a20a1d3318da3d054dd6463
getkam <87501819+getkam@users.noreply.github.com>
2024-03-03 10:46:17 +0000

    Sun, Mar 3, 2024, 11:46 AM +01:00

--- finance/static/scripts.js
+++ finance/static/scripts.js
@@ -3,9 +3,9 @@ function validatePassword(){
     let pass = document.getElementById("password").value
     //let pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    if (pass.length < 5 || !/[a-z]/.test(pass) || !/[A-Z]/.test(pass) || !/[@\.$!%*?&]/.test(pass) || !/d/.test(pass))
+    if (pass.length < 5 || !/[a-z]/.test(pass) || !/[A-Z]/.test(pass) || /[@\.$!%*?&]/.test(pass) || !/d/.test(pass))
     {
-        alert('Password must have at least 5 characters');
+        alert('Password must have at least 5 characters, small letter, capital letter, digit and special sign');
        // document.getElementById("errorPasswordValidation").innerTest = "Password doesn't match criteria"
     }
 }
